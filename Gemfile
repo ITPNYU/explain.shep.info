3440d5138b94932ca7fc814a12f857a13a078b08
Steven Klise <sklise@gmail.com>
2012-08-09 17:34:26 -0400

    It worked with bundle exec

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source :rubygems
 
+gem 'mail'
 gem 'sinatra'
 gem 'kramdown'
 
--- mail.rb
+++ mail.rb
@@ -1,7 +1,8 @@
-require './app'
+require 'bundler'
+Bundler.require
 require 'net/imap'
-require 'mail'
 require 'net/smtp'
+require './models'
 
 imap = Net::IMAP.new('imap.gmail.com', ssl: true)
 # LOGIN
@@ -9,15 +10,23 @@ imap.login(ENV['GMAIL_ADDRESS'], ENV['GMAIL_PASSWORD'])
 # Open up the inbox
 imap.select("INBOX")
 
-# Get message IDs for unread mail.
-unread = imap.search(["NOT","SEEN"])
-
+# Find ids for messages that have "explain" in the subject.
+# Not sure how net/imap handles search, but so far it's working well.
 explain_ids = imap.search(["SUBJECT","explain"])
 
 explain_ids.each do |id|
-  envelope = imap.fetch(id, "ENVELOPE")[0].attr["ENVELOPE"]
-  body = imap.fetch(id, "BODY[TEXT]")[0].attr["BODY[TEXT]"]
-  mail = Mail.new(imap.uid_fetch(2200, "RFC822")[0].attr["RFC822"])
-  # Doesn't detect for signature.
+  # envelope = imap.fetch(id, "ENVELOPE")[0].attr["ENVELOPE"]
+  # body = imap.fetch(id, "BODY[TEXT]")[0].attr["BODY[TEXT]"]
+  msg = imap.fetch(id, "RFC822")[0].attr["RFC822"]
+  # mail = Mail.new(imap.uid_fetch(id, "RFC822")[0].attr["RFC822"])
+  mail = Mail.read_from_string(msg)
   content = mail.body.parts().first.to_s.match(/Content-ID: <.*?>..(.*)/m)[1]
+
+  new_mail = Email.new
+  new_mail.from = mail.from
+  new_mail.subject = mail.subject
+  new_mail.date = mail.date
+  new_mail.body = content
+  new_mail.message_id = mail.message_id
+  puts new_mail.save
 end
